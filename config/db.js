469a5edccee534ee7cca1552614df03cc91957eb
Joey Grisafe <jgrisafe@gmail.com>
2015-06-10 08:47:11 -0700

    updates

--- config/db.js
+++ config/db.js
@@ -1,3 +1,15 @@
 module.exports = {
-	url : 'Your DB url here'
-}
+	url : 'mongodb://user:password@ds039880.mongolab.com:39880/blackjackpro'
+};
+
+// ----- MONGOLABS URI CONNECTION ----------
+
+// mongodb://user:password@ds039880.mongolab.com:39880/blackjackpro
+
+// ----- MONGOLABS Shell CONNECTION ----------
+
+// mongo ds039880.mongolab.com:39880/blackjackpro -u user -p password
+
+// ----- Local host URI CONNECTION ----------
+
+// mongodb://localhost/myapp
